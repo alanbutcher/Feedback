aec621d087e16a8d410e901424353e2343d065a4
alanbutcher <alan.butcher81@gmail.com>
2019-09-18 22:41:36 -0600

    oauthtest

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const passport = require('passport'); //possibly remove
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //possibly remove
+const keys = require('./config/keys');
 
 const init = async () => {
-
-const port = process.env.PORT || 3000;
-
+  
+  const port = process.env.PORT || 5000;
+  
   const server = Hapi.server({
     port: port
     // host: 'localhost'
   });
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-
-      return 'Bye Buddy!';
-    }
-  });
-
-
+  passport.use(
+    new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: 'auth/google/callback'
+    },
+      accessToken => {
+        console.log(accessToken);
+      }
+    )
+  );  
+  
+  
   await server.start();
   console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
+  
   console.log(err);
   process.exit(1);
 });
 
 init();
+
+  // server.route({
+  //   method: 'GET',
+  //   path: '/',
+  //   handler: (request, h) => {
+  
+  //     return 'Bye Buddy!';
+  //   }
+  // });
