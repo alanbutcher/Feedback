bbdc6235fc38d8cd0ff7154c808ed561fd6bd5ee
alanbutcher <alan.butcher81@gmail.com>
2019-09-20 16:02:56 -0600

    push to express

--- index.js
+++ index.js
@@ -1,53 +1,16 @@
-'use strict';
-
-// import HapiPassport from 'passport-hapi-oauth';
-const Hapi = require('@hapi/hapi');
-const passport = require('passport'); //possibly remove
-const GoogleStrategy = require('passport-google-oauth20').Strategy; //possibly remove
-const keys = require('./config/keys');
-
-const init = async () => {
-  
-  const port = process.env.PORT || 5000;
-  
-  const server = Hapi.server({
-    port: port
-  });
+const express = require('express');
+const app = express();
+// const passport = require('passport'); //possibly remove
+// const GoogleStrategy = require('passport-google-oauth20').Strategy; //possibly remove
+// const keys = require('./config/keys');
 
 
 
-  passport.use(
-    new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: 'auth/google/callback'
-    },
-      accessToken => {
-        console.log(accessToken);
-      }
-    )
-  );  
-  
-  server.route({
-    method: 'GET',
-    path: '/auth/google',
-    handler: (request, reply) => {
-      (passport.authenticate('google', {
-        scope: ['profile', 'email']
-      })
-      )
-    }
-
-  
-  await server.start();
-  console.log('Server running on %s', server.info.uri);
-
-
-process.on('unhandledRejection', (err) => {
-  
-  console.log(err);
-  process.exit(1);
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
 });
 
-init();
+const PORT = process.env.PORT || 5000;
+app.listen(POR);
+
+// https://infinite-caverns-55145.herokuapp.com/
