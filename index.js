c52d3320d80b111c3da540fa1a7f6ae028df2db5
alanbutcher <alan.butcher81@gmail.com>
2019-09-23 22:06:12 -0600

    push

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express');
+const passport = require('passport'); 
+const GoogleStrategy = require('passport-google-oauth20').Strategy; 
+const keys = require('./config/keys');
 const app = express();
-// const passport = require('passport'); //possibly remove
-// const GoogleStrategy = require('passport-google-oauth20').Strategy; //possibly remove
-// const keys = require('./config/keys');
+
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile:', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google')
+
+);
 
 
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(POR);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
 // https://infinite-caverns-55145.herokuapp.com/
