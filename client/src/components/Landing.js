b0bebd56bf3667fe618d7349e9ea458d03b3ac74
alanbutcher <alan.butcher81@gmail.com>
2019-10-01 15:33:26 -0600

    stripe added

---
+++ client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//flesh out landing component with styling and marketing
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Feedback!
+      </h1>
+      Collect feedback from oyur users
+    </div>
+
+  )
+}
+
+export default Landing;
